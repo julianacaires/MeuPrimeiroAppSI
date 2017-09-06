1d4def462d1626b7c4193076c16f120aecaae39e
arqdsis <arqdsis@mocli10229.animaedu.intranet>
2017-09-06 20:57:32 -0300

    Initial commit

---
+++ app/src/main/java/br/usjt/ftce/arqdesis/meuprimeiroappsi/MainActivity.java
@@ -0,0 +1,13 @@
+package br.usjt.ftce.arqdesis.meuprimeiroappsi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
